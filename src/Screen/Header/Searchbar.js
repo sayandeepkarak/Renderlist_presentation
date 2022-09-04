e31ae237a3af1a01236269f319478aa846db6142
fuchu2003 <sayandeep18.webdev@gmail.com>
2022-09-04 14:41:55 +0530

    search filter added

--- src/Screen/Header/Searchbar.js
+++ src/Screen/Header/Searchbar.js
@@ -11,30 +11,38 @@ import searchicon from "../../Assets/Images/searchicon.png";
 import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchValue } from "../../App/searchValueSlice";
-// import { searchfilter } from "../../App/allDataSlice";
+import { fetchallplaylists, searchfilter } from "../../App/allDataSlice";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../Firebase";
 
 const Searchbar = () => {
   const dispatch = useDispatch();
   const searchValue = useSelector((state) => state.searchValueReducers.value);
-  // const allPlaylists = useSelector((state) => state.allPlayListReducers.value);
+  const allPlaylists = useSelector((state) => state.allPlayListReducers.value);
 
   const [openSearch, setOpenSearch] = useState(false);
   const handleOpenSearch = () => {
     setOpenSearch(!openSearch);
-    console.log(openSearch);
   };
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     dispatch(setSearchValue(e.target.value));
-    // if (searchValue !== "") {
-    //   dispatch(
-    //     searchfilter(
-    //       allPlaylists.filter((data) => data.Title.includes(searchValue))
-    //     )
-    //   );
-    // } else {
-    //   dispatch(searchfilter(allPlaylists));
-    // }
+    if (e.target.value.length === 0) {
+      try {
+        const data = await getDocs(collection(db, "Playlists"));
+        dispatch(fetchallplaylists(data.docs));
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      dispatch(
+        searchfilter(
+          allPlaylists.filter((data) =>
+            data.Title.toLowerCase().includes(searchValue.toLowerCase())
+          )
+        )
+      );
+    }
   };
 
   return (
